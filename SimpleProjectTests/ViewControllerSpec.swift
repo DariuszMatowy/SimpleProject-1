94565e067ca5b0fb62c7d3d008a6d69f9dab8038
Aleksander Popko <aleksander.popko@netguru.pl>
2015-05-24 14:45:32 +0200

    Add simple test.

---
+++ SimpleProjectTests/ViewControllerSpec.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerSpec.swift
+//  SimpleProject
+//
+//  Created by Aleksander Popko on 24.05.2015.
+//  Copyright (c) 2015 AP. All rights reserved.
+//
+
+import Nimble
+import Quick
+
+class ViewControllerSpec: QuickSpec {
+    
+    override func spec() {
+        
+        let mockNumberFirst = 5;
+        let mockNumberSecond = 3;
+        let mockSum = mockNumberFirst + mockNumberSecond
+        
+        
+        var sut: ViewController!
+        
+        beforeEach {
+            sut = ViewController()
+        }
+        
+        afterEach {
+            sut = nil
+        }
+        
+        it("should return proper sum") {
+            expect(sut.sumOfTwoNumbers(mockNumberFirst, secondNumber: mockNumberSecond)).to(equal(mockSum))
+        }
+    }
+}
